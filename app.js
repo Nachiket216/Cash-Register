92b6b179311e8bd4035ec0bccd56ac67d8c77fa2
Nachiket <nachiket216@gmail.com>
2022-11-29 12:00:57 +0530

    Improved logic and modified input type as number

--- app.js
+++ app.js
@@ -8,12 +8,21 @@ const availableNotes = [2000,500,100,20,10,5,1]
 
 checkButton.addEventListener("click",function validateBillAndCashAmount() {
     hideMessage();
-    if(billAmount.value>0){
-        if(cashGiven.value >= billAmount.value){
+    if(billAmount.value===""){
+        showMessage("Please enter bill amount")
+    }
+    else if(billAmount.value>0){
+        if(cashGiven.value === ""){
+            showMessage("Please enter cash given amount")
+        }
+        else if(cashGiven.value<0){
+            showMessage("Invalid Cash Given Amount")
+        }else if(Number(cashGiven.value)<Number(billAmount.value)){
+            showMessage("The cash provided should atleast be equal to the bill amount")
+        }
+        else{
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
-        }else{
-            showMessage("The cash provided should atleast be equal to the bill amount")
         }
     }else{
         showMessage("Invalid Bill Amount")
